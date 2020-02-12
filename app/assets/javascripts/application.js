87756042df7de324843b0a8620b71d99314a642c
Noah Johnson <noah.b.johnson@dartmouth.edu>
2020-02-12 15:22:31 -0500

    mounted ActionCable and broadcasted chats to index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,9 +17,26 @@
 //= require semantic-ui
 //= require_tree .
 
+scrollBottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  };
+};
+
+submitMessage = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = "";
+    };
+  });
+};
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  submitMessage();
+  scrollBottom();
 })
